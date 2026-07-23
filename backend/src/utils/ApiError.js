d4f87b25eb5a99eee6202a1d9d3672206863693a
Pritam <kumarpreetjangir@gmail.com>
2026-07-23 22:30:56 +0530

    basic architecture and utils done

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,14 @@
+export default class ApiError extends Error {
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = []
+    ) {
+        super(message);
+
+        this.name = "ApiError";
+        this.statusCode = statusCode;
+        this.success = false;
+        this.errors = errors;
+    }
+}
